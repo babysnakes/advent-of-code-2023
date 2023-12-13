33b18a93a0ef08ce5d8d72a2ccaf88eafd12fc26
Haim Ashkenazi <haim@babysnakes.org>
2023-12-13 19:46:41 +0200

    Finished day 1

--- AOC2023/Day01.fs
+++ AOC2023/Day01.fs
@@ -18,6 +18,61 @@ module Part1 =
         let result = input |> compute
         printfn $"Part 1: {result}"
 
+
+module Part2 =
+    let numbers =
+        [ ("one", '1')
+          ("two", '2')
+          ("three", '3')
+          ("four", '4')
+          ("five", '5')
+          ("six", '6')
+          ("seven", '7')
+          ("eight", '8')
+          ("nine", '9') ]
+        |> Map
+
+    let matchesNumber (s: string) =
+        let numStrings = numbers |> Map.keys
+
+        Seq.tryFind (fun (st: string) -> s.EndsWith(st)) numStrings
+        |> Option.bind (fun s -> Map.tryFind s numbers)
+
+    let preparse (chars: char list) =
+        let rec loop lst digits acc =
+            match lst with
+            | [] -> digits |> List.rev
+            | head :: rest when Char.IsDigit head -> loop rest (head :: digits) [||]
+            | head :: rest ->
+                let s = Array.append acc [| head |] |> System.String.Concat
+
+                match matchesNumber s with
+                | Some c -> loop rest (c :: digits) [| head |]
+                | None -> loop rest digits (Array.append acc [| head |])
+
+        loop chars [] [||]
+
+    let getCalibrationValue (line: string) =
+        let chars = Seq.toList line
+        let digits = preparse chars
+        let first = digits |> List.head
+        let last = digits |> List.last
+        $"{first}{last}" |> int
+
+    let compute lines =
+        lines |> Seq.map getCalibrationValue |> Seq.sum
+
+    let run input =
+        let result = input |> compute
+        printfn $"Part 2: {result}"
+
+
 let run (part: Parts) =
     let input = readLines "day01-input.txt"
-    Part1.run input
+
+    match part with
+    | Part1 -> Part1.run input
+    | Part2 -> Part2.run input
+    | Unspecified ->
+        Part1.run input
+        Part2.run input
--- AOCTests/Day01Tests.fs
+++ AOCTests/Day01Tests.fs
@@ -11,3 +11,20 @@ let ``part1 computes correctly`` () =
     let input = readLines "day01-input.txt"
     let result = Part1.compute input
     test <@ result = expected @>
+
+[<Fact>]
+let ``part2 computes correctly`` () =
+    let expected = 56017
+    let input = readLines "day01-input.txt"
+    let result = Part2.compute input
+    test <@ result = expected @>
+
+module Helpers =
+    [<Theory>]
+    [<InlineData("eightwothree", "823")>]
+    [<InlineData("xtwone3four", "2134")>]
+    [<InlineData("4nineeightseven2", "49872")>]
+    [<InlineData("twotsszxmtmvvdldqcdkvbtd7one", "271")>]
+    let ``preparse line works correctly`` (s: string) (expected: string) =
+        let result = s |> Seq.toList |> Part2.preparse
+        test <@ result = (expected |> Seq.toList) @>
