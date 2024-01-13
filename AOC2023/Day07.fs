5bbcc39ad57d52eb3aa0ac0d66613f80d08e3239
Haim Ashkenazi <haim@babysnakes.org>
2024-01-13 22:18:41 +0200

    Day 7 part 2

--- AOC2023/Day07.fs
+++ AOC2023/Day07.fs
@@ -5,79 +5,79 @@ open AOC2023.Models
 open AOC2023.CommonIO
 open AOC2023.Parser
 
-type Card =
-    | Two
-    | Three
-    | Four
-    | Five
-    | Six
-    | Seven
-    | Eight
-    | Nine
-    | Ten
-    | J
-    | Q
-    | K
-    | A
-
-type Cards = Card list
-
-type Hand =
-    | HighCard of Cards
-    | OnePair of Cards
-    | TwoPairs of Cards
-    | ThreeOfKind of Cards
-    | FullHouse of Cards
-    | FourOfKind of Cards
-    | FiveOfKind of Cards
-
-let parseCards cards =
-    match cards |> List.countBy id |> List.sortByDescending snd with
-    | (_, 5) :: _ -> FiveOfKind cards
-    | (_, 4) :: _ -> FourOfKind cards
-    | (_, 3) :: (_, 2) :: _ -> FullHouse cards
-    | (_, 3) :: _ -> ThreeOfKind cards
-    | (_, 2) :: (_, 2) :: _ -> TwoPairs cards
-    | (_, 2) :: _ -> OnePair cards
-    | _ -> HighCard cards
-
-let parseHand (cards, bid) =
-    let hand = parseCards cards
-    (hand, bid)
-
-let pTwo = pchar '2' >>% Two
-let pThree = pchar '3' >>% Three
-let pFour = pchar '4' >>% Four
-let pFive = pchar '5' >>% Five
-let pSix = pchar '6' >>% Six
-let pSeven = pchar '7' >>% Seven
-let pEight = pchar '8' >>% Eight
-let pNine = pchar '9' >>% Nine
-let pTen = pchar 'T' >>% Ten
-let pJack = pchar 'J' >>% J
-let pQueen = pchar 'Q' >>% Q
-let pKing = pchar 'K' >>% K
-let pAce = pchar 'A' >>% A
-
-let pCard =
-    pTwo
-    <|> pThree
-    <|> pFour
-    <|> pFive
-    <|> pSix
-    <|> pSeven
-    <|> pEight
-    <|> pNine
-    <|> pTen
-    <|> pJack
-    <|> pQueen
-    <|> pKing
-    <|> pAce
-
-let pCards = many pCard
-let pLine = pCards .>> spaces .>>. pint32 |>> parseHand
-
 module Part1 =
+    type Card =
+        | Two
+        | Three
+        | Four
+        | Five
+        | Six
+        | Seven
+        | Eight
+        | Nine
+        | Ten
+        | J
+        | Q
+        | K
+        | A
+
+    type Cards = Card list
+
+    type Hand =
+        | HighCard of Cards
+        | OnePair of Cards
+        | TwoPairs of Cards
+        | ThreeOfKind of Cards
+        | FullHouse of Cards
+        | FourOfKind of Cards
+        | FiveOfKind of Cards
+
+    let parseCards cards =
+        match cards |> List.countBy id |> List.sortByDescending snd with
+        | (_, 5) :: _ -> FiveOfKind cards
+        | (_, 4) :: _ -> FourOfKind cards
+        | (_, 3) :: (_, 2) :: _ -> FullHouse cards
+        | (_, 3) :: _ -> ThreeOfKind cards
+        | (_, 2) :: (_, 2) :: _ -> TwoPairs cards
+        | (_, 2) :: _ -> OnePair cards
+        | _ -> HighCard cards
+
+    let parseHand (cards, bid) =
+        let hand = parseCards cards
+        (hand, bid)
+
+    let pTwo = pchar '2' >>% Two
+    let pThree = pchar '3' >>% Three
+    let pFour = pchar '4' >>% Four
+    let pFive = pchar '5' >>% Five
+    let pSix = pchar '6' >>% Six
+    let pSeven = pchar '7' >>% Seven
+    let pEight = pchar '8' >>% Eight
+    let pNine = pchar '9' >>% Nine
+    let pTen = pchar 'T' >>% Ten
+    let pJack = pchar 'J' >>% J
+    let pQueen = pchar 'Q' >>% Q
+    let pKing = pchar 'K' >>% K
+    let pAce = pchar 'A' >>% A
+
+    let pCard =
+        pTwo
+        <|> pThree
+        <|> pFour
+        <|> pFive
+        <|> pSix
+        <|> pSeven
+        <|> pEight
+        <|> pNine
+        <|> pTen
+        <|> pJack
+        <|> pQueen
+        <|> pKing
+        <|> pAce
+
+    let pCards = many pCard
+    let pLine = pCards .>> spaces .>>. pint32 |>> parseHand
+
 
     let compute input =
         input
@@ -92,13 +92,107 @@ module Part1 =
 
 
 module Part2 =
+    type Card =
+        | J
+        | Two
+        | Three
+        | Four
+        | Five
+        | Six
+        | Seven
+        | Eight
+        | Nine
+        | Ten
+        | Q
+        | K
+        | A
 
-    let compute input = 0
+    type Cards = Card list
+
+    type Hand =
+        | HighCard of Cards
+        | OnePair of Cards
+        | TwoPairs of Cards
+        | ThreeOfKind of Cards
+        | FullHouse of Cards
+        | FourOfKind of Cards
+        | FiveOfKind of Cards
+
+    let parseCards cards =
+        let partition = cards |> List.countBy id |> List.sortByDescending snd
+
+        let jokers =
+            partition
+            |> List.tryFind (fun (c, _) -> c = J)
+            |> Option.map snd
+            |> Option.defaultValue 0
+
+        match jokers, partition with
+        | _, (_, 5) :: _ -> FiveOfKind cards
+        | 0, (_, 4) :: _ -> FourOfKind cards
+        | _, (_, 4) :: _ -> FiveOfKind cards
+        | _, (J, 3) :: (_, 2) :: _ -> FiveOfKind cards
+        | _, (_, 3) :: (J, 2) :: _ -> FiveOfKind cards
+        | _, (_, 3) :: (_, 2) :: _ -> FullHouse cards
+        | _, (J, 3) :: _ -> FourOfKind cards
+        | 1, (_, 3) :: _ -> FourOfKind cards
+        | _, (_, 3) :: _ -> ThreeOfKind cards
+        | 2, (_, 2) :: (_, 2) :: _ -> FourOfKind cards
+        | 1, (_, 2) :: (_, 2) :: _ -> FullHouse cards
+        | _, (_, 2) :: (_, 2) :: _ -> TwoPairs cards
+        | 1, (_, 2) :: _ -> ThreeOfKind cards
+        | _, (J, 2) :: _ -> ThreeOfKind cards
+        | _, (_, 2) :: _ -> OnePair cards
+        | 1, _ -> OnePair cards
+        | _, _ -> HighCard cards
+
+    let parseHand (cards, bid) =
+        let hand = parseCards cards
+        (hand, bid)
+
+    let pTwo = pchar '2' >>% Two
+    let pThree = pchar '3' >>% Three
+    let pFour = pchar '4' >>% Four
+    let pFive = pchar '5' >>% Five
+    let pSix = pchar '6' >>% Six
+    let pSeven = pchar '7' >>% Seven
+    let pEight = pchar '8' >>% Eight
+    let pNine = pchar '9' >>% Nine
+    let pTen = pchar 'T' >>% Ten
+    let pJoker = pchar 'J' >>% J
+    let pQueen = pchar 'Q' >>% Q
+    let pKing = pchar 'K' >>% K
+    let pAce = pchar 'A' >>% A
+
+    let pCard =
+        pTwo
+        <|> pThree
+        <|> pFour
+        <|> pFive
+        <|> pSix
+        <|> pSeven
+        <|> pEight
+        <|> pNine
+        <|> pTen
+        <|> pJoker
+        <|> pQueen
+        <|> pKing
+        <|> pAce
+
+    let pCards = many pCard
+    let pLine = pCards .>> spaces .>>. pint32 |>> parseHand
+
+
+    let compute input =
+        input
+        |> List.map (run pLine >> unwrap)
+        |> List.sortBy fst
+        |> List.mapi (fun idx (_, bid) -> bid * (idx + 1))
+        |> List.sum
 
     let run input =
         let result = compute input
         printfn $"Part2: {result}"
-        printfn "NOT IMPLEMENTED"
 
 
 let run (part: Parts) =
--- AOCTests/Day07Tests.fs
+++ AOCTests/Day07Tests.fs
@@ -5,21 +5,6 @@ open AOC2023.Day07
 open AOCTests.TestsHelpers
 open Swensen.Unquote.Assertions
 
-let parseCardsSamples: obj[] list =
-    [ [| [ A; J; Two; Two; Three ]; OnePair [ A; J; Two; Two; Three ] |]
-      [| [ Q; Three; Q; Q; Q ]; FourOfKind [ Q; Three; Q; Q; Q ] |]
-      [| [ J; J; J; J; J ]; FiveOfKind [ J; J; J; J; J ] |]
-      [| [ Four; Six; Q; Six; Four ]; TwoPairs [ Four; Six; Q; Six; Four ] |]
-      [| [ Four; J; Four; Four; A ]; ThreeOfKind [ Four; J; Four; Four; A ] |]
-      [| [ Three; A; J; Four; Ten ]; HighCard [ Three; A; J; Four; Ten ] |]
-      [| [ Three; Three; J; Three; J ]; FullHouse [ Three; Three; J; Three; J ] |] ]
-
-[<Theory>]
-[<MemberData(nameof (parseCardsSamples))>]
-let ``parseCards correctly parses hand`` (hand: Cards, expected: Hand) =
-    let result = parseCards hand
-    test <@ result = expected @>
-
 [<Theory>]
 [<InlineData("day07-example.txt", 6440)>]
 [<InlineData("day07-input.txt", 247961593)>]
@@ -29,9 +14,40 @@ let ``part1 computes correctly`` (input: string) (expected: int) =
     test <@ result = expected @>
 
 [<Theory>]
-[<InlineData("day07-example.txt", 0)>]
-// [<InlineData("day07-input.txt", 0)>]
+[<InlineData("day07-example.txt", 5905)>]
+[<InlineData("day07-input.txt", 248750699)>]
 let ``part2 computes correctly`` (input: string) (expected: int) =
     let lines = readLines input |> List.ofSeq
     let result = Part2.compute lines
     test <@ result = expected @>
+
+module Part1 =
+    open AOC2023.Day07.Part1
+
+    let parseCardsSamples: obj[] list =
+        [ [| [ A; J; Two; Two; Three ]; OnePair [ A; J; Two; Two; Three ] |]
+          [| [ Q; Three; Q; Q; Q ]; FourOfKind [ Q; Three; Q; Q; Q ] |]
+          [| [ J; J; J; J; J ]; FiveOfKind [ J; J; J; J; J ] |]
+          [| [ Four; Six; Q; Six; Four ]; TwoPairs [ Four; Six; Q; Six; Four ] |]
+          [| [ Four; J; Four; Four; A ]; ThreeOfKind [ Four; J; Four; Four; A ] |]
+          [| [ Three; A; J; Four; Ten ]; HighCard [ Three; A; J; Four; Ten ] |]
+          [| [ Three; Three; J; Three; J ]; FullHouse [ Three; Three; J; Three; J ] |] ]
+
+    [<Theory>]
+    [<MemberData(nameof (parseCardsSamples))>]
+    let ``parseCards correctly parses hand`` (hand: Cards, expected: Hand) =
+        let result = parseCards hand
+        test <@ result = expected @>
+
+module Part2 =
+    open AOC2023.Day07.Part2
+
+    let parseCardsSamples: obj[] list =
+        [ [| [ J; J; J; Three; Four ]; FourOfKind [ J; J; J; Three; Four ] |]
+          [| [ J; Six; Eight; Q; J ]; ThreeOfKind [ J; Six; Eight; Q; J ] |] ]
+
+    [<Theory>]
+    [<MemberData(nameof (parseCardsSamples))>]
+    let ``parseCards correctly parses hand`` (hand: Cards, expected: Hand) =
+        let result = parseCards hand
+        test <@ result = expected @>
