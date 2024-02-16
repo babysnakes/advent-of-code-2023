8f08707b905993bfa270c5d8c3d922423e432b9c
Haim Ashkenazi <haim@babysnakes.org>
2024-02-16 09:14:01 +0200

    Day 15 part 2

--- AOC2023/Day15.fs
+++ AOC2023/Day15.fs
@@ -1,15 +1,17 @@
 ﻿module AOC2023.Day15
 
 open System
+open FParsec
 open AOC2023.Models
 open AOC2023.CommonIO
+open AOC2023.Parser
+
+let hashFolder state (c: char) =
+    c |> (int >> (+) state >> (*) 17 >> (fun n -> n % 256))
+
+let applyHASH = Seq.fold hashFolder 0
 
 module Part1 =
-    let hashFolder state (c: char) =
-        c |> (int64 >> (+) state >> (*) 17L >> (fun n -> n % 256L))
-
-    let applyHASH = Seq.fold hashFolder 0L
-
     let compute (input: string) =
         input.Split(',', StringSplitOptions.RemoveEmptyEntries)
         |> Array.map applyHASH
@@ -21,13 +23,59 @@ module Part1 =
 
 
 module Part2 =
+    type Operation =
+        | Minus
+        | Equals of int
 
-    let compute input = 0
+    type State = Map<int, (string * int) list>
+
+    let pLetters = regex "\w+"
+    let parseMinus = pchar '-' >>% Minus
+    let parseEquals = pchar '=' >>. pint32 |>> Equals
+    let pOperation = parseMinus <|> parseEquals
+    let pSequence = pLetters .>>. pOperation
+
+    let lensesFolder (state: State) (label: string, op: Operation) : State =
+        let removeLabel lenses =
+            match List.tryFindIndex (fun (l, _) -> l = label) lenses with
+            | None -> Some lenses
+            | Some idx -> lenses |> List.removeAt idx |> Some
+
+        let addLabel lenses focalLength =
+            match List.tryFindIndex (fun (l, _) -> l = label) lenses with
+            | None -> (label, focalLength) :: lenses |> Some
+            | Some idx ->
+                lenses
+                |> List.mapi (fun i v -> if i = idx then (label, focalLength) else v)
+                |> Some
+
+
+        let change box =
+            match (box, op) with
+            | None, Minus -> None
+            | None, Equals focalLength -> [ (label, focalLength) ] |> Some
+            | Some lenses, Minus -> removeLabel lenses
+            | Some lenses, Equals focalLength -> addLabel lenses focalLength
+
+        state |> Map.change (applyHASH label) change
+
+    let calculateFocusingPower (box: int) (lenses: (string * int) list) =
+        lenses
+        |> List.mapi (fun idx (_, forcalLength) -> (box + 1) * (idx + 1) * forcalLength)
+
+    let compute (input: string) =
+        input.Split(',', StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (run pSequence >> unwrap)
+        |> Array.fold lensesFolder Map.empty
+        |> Map.map (fun _ -> List.rev)
+        |> Map.map calculateFocusingPower
+        |> Map.values
+        |> List.concat
+        |> List.sum
 
     let run input =
         let result = compute input
         printfn $"Part2: {result}"
-        printfn "NOT IMPLEMENTED"
 
 
 let run (part: Parts) =
--- AOCTests/Day15Tests.fs
+++ AOCTests/Day15Tests.fs
@@ -6,17 +6,17 @@ open AOCTests.TestsHelpers
 open Swensen.Unquote.Assertions
 
 [<Theory>]
-[<InlineData("day15-example.txt", 1320L)>]
-[<InlineData("day15-input.txt", 513172L)>]
+[<InlineData("day15-example.txt", 1320)>]
+[<InlineData("day15-input.txt", 513172)>]
 let ``part1 computes correctly`` (input: string) (expected: int) =
     let input = readLines input |> List.ofSeq |> List.head
     let result = Part1.compute input
     test <@ result = expected @>
 
 [<Theory>]
-[<InlineData("day15-example.txt", 0)>]
-// [<InlineData("day15-input.txt", 0)>]
+[<InlineData("day15-example.txt", 145)>]
+[<InlineData("day15-input.txt", 237806)>]
 let ``part2 computes correctly`` (input: string) (expected: int) =
-    let lines = readLines input |> List.ofSeq
-    let result = Part2.compute lines
+    let input = readLines input |> List.ofSeq |> List.head
+    let result = Part2.compute input
     test <@ result = expected @>
